4b0f2ff2ca3c32ec1b425bddfc0057206bdbde7e
bajajtushar094 <bajajtushar2019@gmail.com>
2020-08-19 01:20:04 +0530

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -22,21 +22,21 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 
 
 
-
-void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  await FlutterDownloader.initialize(
-  debug: false // optional: set false to disable printing logs to console
-);
-
-  runApp(MaterialApp(home: HomePage(),
-                     
-  theme: ThemeData(
-    fontFamily: 'Circular'
-  ),
-  ));
-}
+//
+//void main() async {
+//
+//  WidgetsFlutterBinding.ensureInitialized();
+//  await FlutterDownloader.initialize(
+//  debug: false // optional: set false to disable printing logs to console
+//);
+//
+//  runApp(MaterialApp(home: HomePage(),
+//
+//  theme: ThemeData(
+//    fontFamily: 'Circular'
+//  ),
+//  ));
+//}
 
 
 class HomePage extends StatelessWidget {
